6623fda94255c68056cea3c4a16540f89b76d904
dblanchard13 <davidb.prof@gmail.com>
2015-04-20 16:30:11 -0700

    (refactor) Add spaces to improve readability

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var app = require('./server/config.js')
+var app = require('./server/config.js');
+
 var port = process.env.PORT || 8888;
+
 app.listen(port, function() {
   console.log("On port ", port);
-})
-
+});
