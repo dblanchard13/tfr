b2943c6854f8abce6ed32715608b46b9a84d1164
dblanchard13 <davidb.prof@gmail.com>
2015-04-20 15:26:14 -0700

    (fix) Fix process.env variable

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('./server/config.js')
-var port = process.env.production || 8888;
+var port = process.env.PORT || 8888;
 app.listen(port, function() {
   console.log("On port ", port);
 })
